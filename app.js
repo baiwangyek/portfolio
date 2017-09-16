4aa218fd41c6fa7202fba44cc304e55520d63038
baiwang <yekbaiwang93@gmail.com>
2017-09-16 16:10:11 +1200

    implemented transition v1 for fed.html

--- app.js
+++ app.js
@@ -16,71 +16,60 @@ window.addEventListener('load', function(){
   //   svgFill.style.fillOpacity = 0;
   //   svgFill.style.transition = '0.8s';
   // }
+  function baiwangInit() {
+    var svgArr = document.getElementsByTagName('svg');
+    var svgJSON = [
+                  {longestLength: 382, dashOffset: 382},
+                  {longestLength: 448, dashOffset: 448},
+                  {longestLength: 534, dashOffset: 534},
+                  {longestLength: 764, dashOffset: 764},
+                  {longestLength: 504, dashOffset: 504},
+                  {longestLength: 380, dashOffset: 380},
+                  {longestLength: 715, dashOffset: 715},
+                  {longestLength: 501, dashOffset: 501},
+                  {longestLength: 303, dashOffset: 303},
+                  {longestLength: 736, dashOffset: 736}];
+    var longestIndex = 3;
 
-  var svgArr = document.getElementsByTagName('svg');
-  var svgJSON = [
-                {longestLength: 382, dashOffset: 382},
-                {longestLength: 448, dashOffset: 448},
-                {longestLength: 534, dashOffset: 534},
-                {longestLength: 764, dashOffset: 764},
-                {longestLength: 504, dashOffset: 504},
-                {longestLength: 380, dashOffset: 380},
-                {longestLength: 715, dashOffset: 715},
-                {longestLength: 501, dashOffset: 501},
-                {longestLength: 303, dashOffset: 303},
-                {longestLength: 736, dashOffset: 736}];
-  var longestIndex = 3;
-
-  for(var i=0; i<svgArr.length; i++){
-    svgArr[i].style.visibility = 'visible';
-    var svgGroup = svgArr[i].children[2];
-    svgJSON[i].element = svgGroup;
-    // var longestPathLength = getLongestLength(svgGroup);
-    // hideSvgFill(svgGroup);
-    //
-    //hideSvgFill(svgGroup);
-    svgGroup.style.strokeDasharray = svgJSON[i].longestLength;
-    // svgJSON.push({'element': svgGroup, 'longestLength': Math.floor(longestPathLength), 'dashOffset': Math.floor(longestPathLength)});
-    // if(longestPathLength > longestSvgLength){
-    //   longestIndex = i;
-    //   longestSvgLength = longestPathLength;
-    // }
-  }
-
-  //console.log(svgJSON);
-  // console.log(longestIndex);
-
-  function drawSvg(){
-    //console.log('in');
-    if(svgJSON[longestIndex].dashOffset<=0){
-      for(var k=0; k<svgJSON.length; k++){
-        svgJSON[k].element.style.strokeDashoffset = 0; //make sure every svg is fully drawn
-        svgJSON[k].element.children[0].children[0].style.fillOpacity = 1; //show fill
-      }
-
-      document.querySelector('.baiwang__hero-text-container').style.opacity = '1';
-
+    for(var i=0; i<svgArr.length; i++){
+      svgArr[i].style.visibility = 'visible';
+      var svgGroup = svgArr[i].children[2];
+      svgJSON[i].element = svgGroup;
+      svgGroup.style.strokeDasharray = svgJSON[i].longestLength;
     }
-    else{
-      for(var j=0; j<svgJSON.length; j++){
-        if(svgJSON[j].dashOffset<=0){
-          svgJSON[j].element.style.strokeDashoffset = 0;
+
+    function drawSvg(){
+      if(svgJSON[longestIndex].dashOffset<=0){
+        for(var k=0; k<svgJSON.length; k++){
+          svgJSON[k].element.style.strokeDashoffset = 0; //make sure every svg is fully drawn
+          svgJSON[k].element.children[0].children[0].style.fillOpacity = 1; //show fill
         }
-        else{
-          svgJSON[j].element.style.strokeDashoffset = svgJSON[j].dashOffset;
-          svgJSON[j].dashOffset-=8;
-          if(svgJSON[j].dashOffset<0){
-            svgJSON[j].dashOffset=0;
+
+        document.querySelector('.baiwang__hero-text-container').style.opacity = '1';
+
+      }
+      else{
+        for(var j=0; j<svgJSON.length; j++){
+          if(svgJSON[j].dashOffset<=0){
+            svgJSON[j].element.style.strokeDashoffset = 0;
+          }
+          else{
+            svgJSON[j].element.style.strokeDashoffset = svgJSON[j].dashOffset;
+            svgJSON[j].dashOffset-=8;
+            if(svgJSON[j].dashOffset<0){
+              svgJSON[j].dashOffset=0;
+            }
           }
         }
+        requestAnimationFrame(drawSvg);
       }
-      requestAnimationFrame(drawSvg);
     }
+
+    requestAnimationFrame(drawSvg);
   }
 
-  requestAnimationFrame(drawSvg);
-
-  // var lightBulb = svgArr[0].children[2].children[0].children[0];
-
+  if(document.querySelector('.baiwang__body-container')){
+    baiwangInit();
+  }
 
 });
