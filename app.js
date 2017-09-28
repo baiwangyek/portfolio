1fbd54fb2ae2f544852eac4de2f391477d23edc8
baiwang <yekbaiwang93@gmail.com>
2017-09-28 15:17:21 +1300

    updated you.html

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 window.addEventListener('load', function(){
 
+  var submitted=false;
+
   function youInit(){
     if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 
@@ -73,7 +75,7 @@ window.addEventListener('load', function(){
     function checkMobile() {
       var check = false;
       (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-      console.log(check);
+      //console.log(check);
 
       return check;
     };
@@ -192,6 +194,15 @@ window.addEventListener('load', function(){
               //recalculate scroll to reveal points
               updateBodySectionChildren();
 
+              //activate input focus
+              inputs = document.getElementsByClassName('you__contact-input');
+
+              for(var a=0; a<inputs.length; a++){
+                inputs[a].addEventListener('focus', function(){
+                  event.target.parentNode.children[1].classList.add('you__contact-input-placeholder--active');
+                });
+              }
+
             }, 400);
           }, delay);
 
@@ -246,6 +257,20 @@ window.addEventListener('load', function(){
     }
   });
 
+  // document.body.addEventListener('focus', function(){
+  //   console.log('in');
+  //   if(event.target.className.match('you__contact-input')){
+  //     event.target.parentNode.children[1].classList.add('you__contact-input-placeholder--active');
+  //   }
+  // });
+
+  var inputs = document.getElementsByClassName('you__contact-input');
+
+  for(var a=0; a<inputs.length; a++){
+    inputs[a].addEventListener('focus', function(){
+      event.target.parentNode.children[1].classList.add('you__contact-input-placeholder--active');
+    });
+  }
 
   /*************************
   CLICK FUNCTIONS
@@ -253,6 +278,9 @@ window.addEventListener('load', function(){
   var iamNav; //for nav animation and next page animation
 
   document.body.addEventListener('click', function(){
+
+    //reset
+    //if(document.querySelector('.you__email-reveal')){document.querySelector('.you__email-reveal').classList.remove('you__email-reveal--active');}
 
     if(event.target.className.match('right') || event.target.parentNode.className.match('right') || event.target.parentNode.parentNode.className.match('right')){
 
@@ -315,6 +343,75 @@ window.addEventListener('load', function(){
       //ajax-replace
       loadContent(href);
 
+    }
+
+    else if(event.target.className.match('you__contact-input')){
+      event.target.parentNode.children[0].focus();
+    }
+
+    else if(event.target.id === 'emailReveal'){
+      event.preventDefault();
+      document.querySelector('.you__email-reveal').classList.toggle('you__email-reveal--active');
+    }
+
+    else if(event.target.id === 'formSubmit'){
+      event.preventDefault();
+      var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      var counter = 0;
+      //name
+      if(document.getElementById('entry.1345338267').value === ''){
+        console.log('name empty');
+        document.getElementById('nameEmpty').classList.add('form-validation--active');
+        //document.getElementById('entry.1345338267').classList.add('you__input-error');
+      }
+      else {
+        counter++;
+        document.getElementById('nameEmpty').classList.remove('form-validation--active');
+      }
+      //email
+      if(document.getElementById('entry.1428414085').value === ''){
+        document.getElementById('emailEmpty').classList.add('form-validation--active');
+      }
+      else if(!re.test(document.getElementById('entry.1428414085').value)){
+        document.getElementById('emailEmpty').classList.remove('form-validation--active');
+        document.getElementById('emailWrong').classList.add('form-validation--active');
+      }
+      else {
+        counter++;
+        document.getElementById('emailEmpty').classList.remove('form-validation--active');
+        document.getElementById('emailWrong').classList.remove('form-validation--active');
+      }
+      //message
+      if(document.getElementById('entry.791260553').value === ''){
+        document.getElementById('messageEmpty').classList.add('form-validation--active');
+      }
+      else {
+        counter++;
+        document.getElementById('messageEmpty').classList.remove('form-validation--active');
+      }
+
+      if(counter === 3){
+        //document.getElementById('gform').submit();
+
+        event.target.classList.remove('you__contact--active');
+        //event.target.style.display = 'none';
+        document.querySelector('.you__contact--sending').classList.add('you__contact--active');
+
+        window.setTimeout(function(){
+          document.querySelector('.you__contact--sending').classList.remove('you__contact--active');
+          //document.querySelector('.you__contact--sending').style.display = 'none';
+          document.querySelector('.you__contact--sent').classList.add('you__contact--active');
+          document.querySelector('.emoji-sent').style.transform = 'scale(1)';
+        }, 2000);
+
+        window.setTimeout(function(){
+          document.getElementById('entry.1345338267').value = '';
+          document.getElementById('entry.1428414085').value = '';
+          document.getElementById('entry.791260553').value = '';
+          document.querySelector('.you__contact--sent').classList.remove('you__contact--active');
+          document.querySelector('.you__contact--again').classList.add('you__contact--active');
+        }, 4000);
+      }
     }
   });
 });
